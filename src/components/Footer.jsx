91750328d51bf228dc220558826037d4383b51c5
mrhl082024 <mrhl082024@gmail.com>
2025-03-18 12:24:05 +0100

    ADD Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
+import "../styles/Footer.css";
 function Footer() {
   return (
     <>
       <footer id="footer">
         This was made using React. <br />
         <a href="https://github.com/mrhl082024" target="_blank">
-          <img src="public\images\github-mark-white.png" alt="github_logo" />
+          <img
+            id="github-logo"
+            src="public\images\github-mark-white.png"
+            alt="github_logo"
+          />
         </a>
       </footer>
     </>
