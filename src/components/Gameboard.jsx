72e3c109a93516dfca4ec78283709e0fab43a0ab
mrhl082024 <mrhl082024@gmail.com>
2025-03-19 15:00:56 +0100

    savepoint

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,23 +1,41 @@
 import { useState } from "react";
 import "../styles/Gameboard.css";
 
-function Gameboard() {
-  const [playerTurn, setPlayerTurn] = useState();
+function Gameboard({ player, setPlayer }) {
   const handleTurns = () => {
-    setPlayerTurn((e) => !e);
+    setPlayer((e) => !e);
   };
+  const [gameboard, setGameboard] = useState([
+    "test",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
+
   return (
     <>
       <div id="gameboard">
-        <div className="game-box" id="box-1"></div>
-        <div className="game-box" id="box-2"></div>
-        <div className="game-box" id="box-3"></div>
-        <div className="game-box" id="box-4"></div>
-        <div className="game-box" id="box-5"></div>
-        <div className="game-box" id="box-6"></div>
-        <div className="game-box" id="box-7"></div>
-        <div className="game-box" id="box-8"></div>
-        <div className="game-box" id="box-9"></div>
+        {gameboard.map((tile, id) => (
+          <div
+            className="game-box"
+            id={"box" + id}
+            key={id}
+            onClick={() => {
+              setGameboard(
+                gameboard.map((maptile, i) => (i === id ? "x" : maptile))
+              );
+              handleTurns();
+              console.log(player);
+            }}
+          >
+            {tile}
+          </div>
+        ))}
       </div>
     </>
   );
