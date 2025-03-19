dd98dc63a940d91be62c713dfb6cdd70d661f0d3
mrhl082024 <mrhl082024@gmail.com>
2025-03-19 14:09:55 +0100

    ADD Gameboard

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "../styles/Gameboard.css";
 
 function Gameboard() {
   const [playerTurn, setPlayerTurn] = useState();
@@ -8,15 +9,15 @@ function Gameboard() {
   return (
     <>
       <div id="gameboard">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className="game-box" id="box-1"></div>
+        <div className="game-box" id="box-2"></div>
+        <div className="game-box" id="box-3"></div>
+        <div className="game-box" id="box-4"></div>
+        <div className="game-box" id="box-5"></div>
+        <div className="game-box" id="box-6"></div>
+        <div className="game-box" id="box-7"></div>
+        <div className="game-box" id="box-8"></div>
+        <div className="game-box" id="box-9"></div>
       </div>
     </>
   );
