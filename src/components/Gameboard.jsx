05d5af6e5a0d4eda1b91d1ea4a0a1dd44d76ba9b
mrhl082024 <mrhl082024@gmail.com>
2025-03-20 10:32:50 +0100

    ADD Gameboard

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -27,10 +27,29 @@ function Gameboard({ player, setPlayer, setGameStarted }) {
     console.log(gameboard);
   }
   function whoWins() {
-    if (gameboard[(0, 1, 2)] === "X") {
+    if (
+      (gameboard[0] === "X" && gameboard[1] === "X" && gameboard[2] === "X") ||
+      (gameboard[3] === "X" && gameboard[4] === "X" && gameboard[5] === "X") ||
+      (gameboard[6] === "X" && gameboard[7] === "X" && gameboard[8] === "X") ||
+      (gameboard[0] === "X" && gameboard[3] === "X" && gameboard[6] === "X") ||
+      (gameboard[1] === "X" && gameboard[4] === "X" && gameboard[7] === "X") ||
+      (gameboard[2] === "X" && gameboard[5] === "X" && gameboard[8] === "X") ||
+      (gameboard[0] === "X" && gameboard[4] === "X" && gameboard[8] === "X") ||
+      (gameboard[2] === "X" && gameboard[4] === "X" && gameboard[6] === "X")
+    ) {
       setMessage("X Wins!");
-      console.log("HES HIM!");
-      console.log(message);
+    }
+    if (
+      (gameboard[0] === "O" && gameboard[1] === "O" && gameboard[2] === "O") ||
+      (gameboard[3] === "O" && gameboard[4] === "O" && gameboard[5] === "O") ||
+      (gameboard[6] === "O" && gameboard[7] === "O" && gameboard[8] === "O") ||
+      (gameboard[0] === "O" && gameboard[3] === "O" && gameboard[6] === "O") ||
+      (gameboard[1] === "O" && gameboard[4] === "O" && gameboard[7] === "O") ||
+      (gameboard[2] === "O" && gameboard[5] === "O" && gameboard[8] === "O") ||
+      (gameboard[0] === "O" && gameboard[4] === "O" && gameboard[8] === "O") ||
+      (gameboard[2] === "O" && gameboard[4] === "O" && gameboard[6] === "O")
+    ) {
+      setMessage("O Wins!");
     }
   }
 
